0d8c33b70b5fbdb516fa5c028edd1b688345cf26
Igor Alves da Silva <igor.silva@avansys.com.br>
2019-09-24 15:15:57 -0300

    V2 arch

--- src/modules/product/product.controller.ts
+++ src/modules/product/product.controller.ts
@@ -1,21 +1,28 @@
-import { Controller, Get, Post, Res, HttpStatus } from '@nestjs/common';
+import { Controller, Get, Post, Res, HttpStatus, Body } from '@nestjs/common';
 import { Response } from 'express';
+import { ProductService } from './product.service';
+import { Product } from './interfaces/product.interface';
 
 @Controller('product')
 export class ProductController {
+
+  constructor(private readonly productService: ProductService) {}
+
   @Post()
-  create(@Res() res: Response) {
+  async create(@Res() res: Response, @Body() createProductDto: Product) {
+    this.productService.create(createProductDto);
     res.status(HttpStatus.CREATED).send();
   }
 
   @Get()
-  findAll(@Res() res: Response) {
-    res.status(HttpStatus.OK).json([]);
+  async findAll(@Res() res: Response) {
+    return res.status(HttpStatus.OK).json(this.productService.findAll());
   }
 
   @Get('/:id')
   findById(@Res() res: Response, req: Request) {
     res.status(HttpStatus.OK).send({ req });
   }
+ 
 
 }
--- src/modules/product/product.service.ts
+++ src/modules/product/product.service.ts
@@ -1,19 +1,18 @@
-import { Model } from 'mongoose';
-import { Injectable, Inject } from '@nestjs/common';
-// import { InjectModel } from '@nestjs/mongoose';
+import { Injectable } from '@nestjs/common';
 import { Product } from './interfaces/product.interface';
 
 @Injectable()
 export class ProductService {
-  constructor(@Inject('PRODUCT_MODEL') private readonly productModel: Model<any>) {}
 
-  async create(): Promise<Product> {
-    const createdProd = new this.productModel();
-    return await createdProd.save();
+  private readonly products: Product[] = [];
+
+
+  async create(product: Product) {
+    return await this.products.push(product);
   }
 
-  async findAll(): Promise<Product[]> {
-    return await this.productModel.find().exec();
+  findAll(): Product[] {
+    return this.products;
   }
   
 }
