5e6f8191336be034ecca3e8080c745f61422adfa
Igor Alves da Silva <igor.silva@avansys.com.br>
2019-09-24 11:46:49 -0300

    Implementado Webpack hotreload!

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+declare const module: any;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.listen(3000);
-  Promise.all([app])
+  Promise.all([app]);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 
 (async () => {
   await bootstrap();
 })();
 
+// "start": "ts-node -r tsconfig-paths/register src/main.ts",
 
